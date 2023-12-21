6ee8c8d8b81b654e10f0530d63824022c6da4fbd
Douglas Silva <douglas.silva@orionconsult.com.br>
2023-12-21 13:36:59 -0300

    API Rest 1

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request
+import urllib.request, json
+
 app = Flask(__name__)
 
 frutas = []
 registros=[]
+
+#http://image.tmdb.org/t/p/w500/hZkgoQYus5vegHoetLkCJzb17zJ.jpg
 
 @app.route('/', methods=["GET", "POST"])
 def principal():
@@ -21,6 +25,15 @@ def sobre():
 			registros.append({"aluno": request.form.get("aluno"),"nota": request.form.get("nota")})
 
 	return render_template("sobre.html", registros=registros)
+
+@app.route('/filmes')
+def filmes():
+	url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e1f0350b7743692640280055df5d89aa"
+	resposta = urllib.request.urlopen(url)
+	dados = resposta.read()
+	jsondata = json.loads(dados)
+
+	return render_template("filmes.html", filmes=jsondata['results'])
 
 if __name__ =="__main__":
     app.run(debug=True)
--- teste.py
+++ teste.py
@@ -1,10 +1,13 @@
-notas = {
-    "Fulano":5.0,
-    "Beltrano":6.0,
-    "Douglas":7.0,
-    "Queila":5.0
-}
+#https://api.themoviedb.org/4/discover/movie?sort_by=popularity.desc&api_key=e1f0350b7743692640280055df5d89aa
+#Filmes populares
+import urllib.request, json
 
-for chave, valor in notas.items():
-    print(chave)
-    print(valor)
+url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e1f0350b7743692640280055df5d89aa"
+
+resposta = urllib.request.urlopen(url)
+
+dados = resposta.read()
+
+jsondata = json.loads(dados)
+
+print(jsondata['results'])
